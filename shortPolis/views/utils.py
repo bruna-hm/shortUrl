9cc800fc9e1dba3be4c768a78caccfedf430f26a
Bruna <brunaahm@gmail.com>
2025-07-24 20:18:13 -0300

    style: diminui caracteres

--- shortPolis/views/utils.py
+++ shortPolis/views/utils.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-def encurtar_url(tamanho=6):
+def encurtar_url(tamanho=3):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=tamanho))
 
     
